aca478d5247030474f38bdb8bf4abd4f4c2f7082
benzhao <benzhao2866@gmail.com>
2016-05-24 20:27:22 +1200

    add header to index.html

---
+++ themes/DA/js/global.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+var main = function() {
+	$('.navbar .dropdown').hover(
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideDown();
+		}, 
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+		}
+	);
+};
+
+$(document).ready(main);
