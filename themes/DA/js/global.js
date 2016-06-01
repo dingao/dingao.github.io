2b4e92e11c578be06d220239b9a5ccd173000670
benzhao <benzhao2866@gmail.com>
2016-06-02 10:22:38 +1200

    brand new homepage

---
+++ themes/DA/js/global.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+/*
+var main = function() {
+	$('.navbar .dropdown').hover(
+		function() {
+			$(this).find('.dropdown-menu').slideDown();
+		}, 
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+		}
+	);
+};
+
+$(document).ready(main);
+*/
